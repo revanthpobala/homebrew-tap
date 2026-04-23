cb9e508e61e5e42d7b4a6de2d72f0a87238a9430
Revanth <revanthpobala@users.noreply.github.com>
2026-04-22 21:52:26 -0600

    Update devin-cli to 1.3.3

--- Formula/devin-cli.rb
+++ Formula/devin-cli.rb
@@ -1,8 +1,8 @@
 class DevinCli < Formula
   desc "Unofficial CLI for Devin AI"
   homepage "https://github.com/revanthpobala/devin-cli"
-  url "https://files.pythonhosted.org/packages/source/d/devin-cli/devin_cli-1.3.2.tar.gz"
-  sha256 "c393a8da3500bef7df85fdc30dc80b96c07fab918c5443d0849cef05c475b0c0"
+  url "https://files.pythonhosted.org/packages/source/d/devin-cli/devin_cli-1.3.3.tar.gz"
+  sha256 "6d86b6dc89fc967d62f5fec255003d82cdd586d16618db7c23d5b57993824d97"
   license "MIT"
 
   depends_on "python@3.12"
